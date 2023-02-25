63f55e972c6c488f692769bf58ec5c3cb8572e6d
shadowfaz16 <erikfazju@gmail.com>
2023-02-25 09:37:04 -0600

    ignored

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,7 +10,7 @@ const App = () => {
     <Router>
     <header className="w-full flex justify-between items-center bg-white sm:px-16 px-4 py-4 border-b border-b-gray-300">
       <Link to="/">
-        <img src={logo} alt="logo" className="w-52 md:w-80 object-contain" />
+        <img src={logo} alt="logo" className="w-56 md:w-80 object-contain" />
       </Link>
       <Link to="/create-tattoo" className="font-inter font-medium bg-emerald-400 text-white text-xs md:text-sm px-4 py-2 rounded-md">
         Create Tattoo
