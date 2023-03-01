b8f77f3b1a484a76e5ce271cb60be8dc50286e45
shadowfaz16 <erikfazju@gmail.com>
2023-02-28 22:08:55 -0700

    lazyload off

--- client/src/components/AnimatedRoutes.jsx
+++ client/src/components/AnimatedRoutes.jsx
@@ -1,25 +1,28 @@
 import React, {lazy, Suspense} from 'react'
 import { useLocation, Route, Routes } from 'react-router-dom'
-const Home = lazy(() => import("../pages/Home"));
-const CreatePost = lazy(() => import("../pages/CreatePost"));
-const Community = lazy(() => import("../pages/Community"));
-const Profile = lazy(() => import("../pages/profile/[id]"));
+import { Home, CreatePost, Community } from "../pages"
+import Profile from "../pages/profile/[id]"
+
+// const Home = lazy(() => import("../pages/Home"));
+// const CreatePost = lazy(() => import("../pages/CreatePost"));
+// const Community = lazy(() => import("../pages/Community"));
+// const Profile = lazy(() => import("../pages/profile/[id]"));
 
 import {AnimatePresence} from 'framer-motion'
 
 const AnimatedRoutes = () => {
   const location = useLocation();
   return (
-    <Suspense fallback={<div>loading...</div>}>
     <AnimatePresence mode='wait'>
+      {/* <Suspense fallback={<div>loading...</div>}> */}
       <Routes location={location} key={location.pathname}>
           <Route path="/" element={<Home />} />
           <Route path="/create-tattoo" element={<CreatePost />} />
           <Route path="/community" element={<Community />} />
           <Route path="/profile/:id" element={<Profile />} />
       </Routes>
+      {/* </Suspense> */}
     </AnimatePresence>
-    </Suspense>
   )
 }
 
